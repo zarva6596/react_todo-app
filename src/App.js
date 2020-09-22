d6812ab26901a693c0c9c9e556deb8914ce07d2a
Zarva <zarva6596@gmail.com>
2020-09-23 01:55:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { CreateTodo } from './components/CreateTodo';
 
 function App() {
   const [todos, pushTodo] = useState([]);
+  const [show, setShow] = useState('all');
 
   const setTodos = (title) => {
     pushTodo([
@@ -17,6 +18,79 @@ function App() {
     ]);
   };
 
+  const handleCompleted = (event) => {
+    pushTodo(todos.map(todo => (
+      todo.id === +event.target.value
+        ? {
+          ...todo,
+          completed: !todo.completed,
+        }
+        : { ...todo }
+    )));
+  };
+
+  const deleteTodo = (event) => {
+    pushTodo(todos.filter(todo => todo.title !== event.target.value));
+  };
+
+  const editTodo = (id) => {
+    pushTodo(todos.map(todo => (
+      todo.id === id
+        ? {
+          ...todo,
+          edit: true,
+        }
+        : { ...todo }
+    )));
+  };
+
+  const handleTodoTitle = (event, id) => {
+    pushTodo(todos.map(todo => (
+      todo.id === id
+        ? {
+          ...todo,
+          title: event.target.value,
+        }
+        : { ...todo }
+    )));
+  };
+
+  const saveChangeTitle = (event, id, outside) => {
+    (outside || event.key === 'Enter') && (event.target.value === ''
+      ? deleteTodo(event)
+      : pushTodo(todos.map(todo => (
+        todo.id === id
+          ? {
+            id: todo.id,
+            title: event.target.value,
+            completed: todo.completed,
+          }
+          : { ...todo }
+      ))));
+  };
+
+  const allCompleted = () => {
+    if (todos.some(todo => !todo.completed)) {
+      pushTodo(todos.map(todo => ({
+        ...todo,
+        completed: true,
+      })));
+    } else {
+      pushTodo(todos.map(todo => ({
+        ...todo,
+        completed: false,
+      })));
+    }
+  };
+
+  const deleteCompletedTodos = () => {
+    pushTodo(todos.filter(todo => !todo.completed));
+  };
+
+  const filterTodos = (event) => {
+    setShow(event.target.value);
+  };
+
   return (
     <section className="todoapp">
       <CreateTodo
@@ -25,73 +99,84 @@ function App() {
       />
 
       <section className="main">
-        <input type="checkbox" id="toggle-all" className="toggle-all" />
-        <label htmlFor="toggle-all">Mark all as complete</label>
+        {todos.length > 0 && (
+          <>
+            <input
+              type="checkbox"
+              id="toggle-all"
+              checked={todos.every(todo => todo.completed)}
+              className="toggle-all"
+              onChange={allCompleted}
+            />
+            <label htmlFor="toggle-all">Mark all as complete</label>
+          </>
+        )}
 
-        {/* <ul className="todo-list">
-          <li>
-            <div className="view">
-              <input type="checkbox" className="toggle" />
-              <label>asdfghj</label>
-              <button type="button" className="destroy" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-
-          <li className="completed">
-            <div className="view">
-              <input type="checkbox" className="toggle" />
-              <label>qwertyuio</label>
-              <button type="button" className="destroy" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-
-          <li className="editing">
-            <div className="view">
-              <input type="checkbox" className="toggle" />
-              <label>zxcvbnm</label>
-              <button type="button" className="destroy" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-
-          <li>
-            <div className="view">
-              <input type="checkbox" className="toggle" />
-              <label>1234567890</label>
-              <button type="button" className="destroy" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-        </ul> */}
-
-        <TodoList todos={todos} />
+        <TodoList
+          todos={todos}
+          handleCompleted={handleCompleted}
+          deleteTodo={deleteTodo}
+          editTodo={editTodo}
+          handleTodoTitle={handleTodoTitle}
+          saveChangeTitle={saveChangeTitle}
+          show={show}
+        />
       </section>
 
-      <footer className="footer">
-        <span className="todo-count">
-          3 items left
-        </span>
+      {todos.length > 0 && (
+        <footer className="footer">
+          <span className="todo-count">
+            {todos.filter(todo => !todo.completed).length}
+            {' '}
+            items left
+          </span>
 
-        <ul className="filters">
-          <li>
-            <a href="#/" className="selected">All</a>
-          </li>
+          <ul className="filters">
+            <li>
+              <button
+                className={show === 'all' ? 'selected' : ''}
+                type="button"
+                value="all"
+                onClick={filterTodos}
+              >
+                All
+              </button>
+            </li>
 
-          <li>
-            <a href="#/active">Active</a>
-          </li>
+            <li>
+              <button
+                className={show === 'active' ? 'selected' : ''}
+                type="button"
+                value="active"
+                onClick={filterTodos}
+              >
+                Active
+              </button>
+            </li>
 
-          <li>
-            <a href="#/completed">Completed</a>
-          </li>
-        </ul>
+            <li>
+              <button
+                className={show === 'completed' ? 'selected' : ''}
+                value="completed"
+                type="button"
+                onClick={filterTodos}
+              >
+                Completed
+              </button>
+            </li>
+          </ul>
 
-        <button type="button" className="clear-completed">
-          Clear completed
-        </button>
-      </footer>
+          {todos.some(todo => todo.completed) && (
+            <button
+              type="button"
+              className="clear-completed"
+              onClick={deleteCompletedTodos}
+            >
+              Clear completed
+            </button>
+          )}
+        </footer>
+      )}
     </section>
   );
 }
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,21 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const TodoList = ({ todos }) => (
+export const TodoList = ({
+  todos,
+  handleCompleted,
+  deleteTodo,
+  editTodo,
+  handleTodoTitle,
+  saveChangeTitle,
+  show,
+}) => (
   <ul className="todo-list">
-    {todos.map(todo => (
-      <li key={todo.id}>
-        <div className="view">
-          <input type="checkbox" className="toggle" />
-          <label>{todo.title}</label>
-          <button type="button" className="destroy" />
-        </div>
-        <input type="text" className="edit" />
-      </li>
-    ))}
+    {todos.filter((todo) => {
+      switch (show) {
+        case 'completed':
+          return todo.completed;
+        case 'active':
+          return !todo.completed;
+        default:
+          return true;
+      }
+    })
+      .map(todo => (
+        <li
+          key={todo.id}
+          className={`${todo.edit && 'editing'}
+          ${todo.completed && 'completed'}`}
+        >
+          <div className="view">
+            <input
+              type="checkbox"
+              className="toggle"
+              value={todo.id}
+              onChange={handleCompleted}
+              checked={todo.completed && true}
+            />
+            <label
+              onDoubleClick={() => editTodo(todo.id)}
+            >
+              {todo.title}
+            </label>
+            <button
+              value={todo.title}
+              type="button"
+              className="destroy"
+              onClick={deleteTodo}
+            />
+          </div>
+          <input
+            type="text"
+            className="edit"
+            value={todo.title}
+            onChange={event => handleTodoTitle(event, todo.id)}
+            onKeyPress={event => saveChangeTitle(event, todo.id)}
+            onBlur={event => saveChangeTitle(event, todo.id, true)}
+          />
+        </li>
+      ))}
   </ul>
 );
 
 TodoList.propTypes = {
   todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+  handleCompleted: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+  editTodo: PropTypes.func.isRequired,
+  handleTodoTitle: PropTypes.func.isRequired,
+  saveChangeTitle: PropTypes.func.isRequired,
+  show: PropTypes.string.isRequired,
 };
